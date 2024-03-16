effc5d8d5ac18198daf7e0343ab81726681cef0a
Muhammad Fuzail <159969929+MuhammadFuzail591@users.noreply.github.com>
2024-03-17 01:06:49 +0500

    Add files via upload

---
+++ Day 2 Challenge/ProgramNo2.ts
@@ -0,0 +1,7 @@
+//Question 2: Famous Quote 2: Repeat Exercise 1, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+
+let famous_person:string="Quaid-E-Azam"
+
+let Message:string=`${famous_person} once said, "Work, Work and only Work"`
+
+console.log(Message)
---
+++ Day 2 Challenge/ProgramNo3.ts
@@ -0,0 +1,9 @@
+//Question 3: Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. Use "\t" (tab) and "\n" (new line) at least once. Print the name once, so the whitespace around the name is displayed. Then print the name after stripping the white spaces.
+
+let Name:string="\t  \n \t Muhammad Fuzail\n"
+
+console.log(`The Name with spaces: ${Name}`)
+
+Name=Name.trim()
+
+console.log(`The Name after removing spaces: ${Name}`)
